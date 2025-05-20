0228fd23f02d82e431b46e48c4244fb90db3cd9e
AdityaDabgotra <adityadabgotra2004@gmail.com>
2025-05-20 07:52:14 +0530

    all trav

---
+++ Take_U_Forward/TREE/all_traversal_once.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+void allTraversal(TreeNode* root){
+
+}
+int main(){
+    TreeNode* tree = new TreeNode(1);
+    tree->left = new TreeNode(2);
+    tree->right = new TreeNode(3);
+    tree->left->left = new TreeNode(4);
+    tree->left->right = new TreeNode(5);
+    tree->left->right->left = new TreeNode(6);
+    tree->left->right->right = new TreeNode(7);
+
+    allTraversal(tree);
+    return 0;
+}
