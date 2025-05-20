18ee033d1c05cb47731553a1e26c003ee2f9a6d7
AdityaDabgotra <adityadabgotra2004@gmail.com>
2025-05-20 13:29:14 +0530

    all traversal in one traversal

--- Take_U_Forward/TREE/all_traversal_once.cpp
+++ Take_U_Forward/TREE/all_traversal_once.cpp
@@ -9,7 +9,37 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-void allTraversal(TreeNode* root){
+vector<vector<int>> allTraversal(TreeNode* root){
+    stack<pair<TreeNode*,int>>st;
+    vector<int>pre;
+    vector<int>in;
+    vector<int>post;
+    st.push({root,1});
+    while (!st.empty()){
+        auto &itr = st.top();
+        TreeNode* node = itr.first;
+        int state = itr.second;
+        if(state == 1){
+            st.pop();
+            pre.push_back(node->val);
+            state++;
+            st.push({node,state});
+            if(node->left)st.push({node->left,1});
+        }
+        else if (state == 2){
+            st.pop();
+            in.push_back(node->val);
+            state++;
+            st.push({node,state});
+            if(node->right)st.push({node->right,1});
+        }
+        else{
+            st.pop();
+            post.push_back(node->val);
+        }
+    }
+    return {pre,in,post};
+    
 
 }
 int main(){
@@ -21,6 +51,14 @@ int main(){
     tree->left->right->left = new TreeNode(6);
     tree->left->right->right = new TreeNode(7);
 
-    allTraversal(tree);
+    vector<vector<int>>result = allTraversal(tree);
+
+    for (int i = 0; i < result.size(); i++){
+        for (int j = 0; j < result[i].size(); j++){
+            cout<<result[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
     return 0;
 }
