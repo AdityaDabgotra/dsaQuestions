a9bb3ad9787c2a095488927b622ca999e0e7ba1e
AdityaDabgotra <adityadabgotra2004@gmail.com>
2025-08-02 22:21:54 +0530

    graph intro

--- Take_U_Forward/GRAPH/graph_intro.cpp
+++ Take_U_Forward/GRAPH/graph_intro.cpp
@@ -7,15 +7,28 @@ int main(){
     cin>>n;
     cout<<"Enter the Number of Edges:\t";
     cin>>m;
+
+    //adjacency matrix 
     int adj[n+1][m+1];
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++){
         int u,v;
         cout<<"Enter the connection:\t";
         cin>>u>>v;
         adj[u][v] = 1;
         adj[v][u] = 1;
     }
+
+    // adjacency list
+    vector<int>adj2[n+1];
+    for (int i = 0; i < m; i++)
+    {
+        int u,v;
+        cout<<"Enter the connection:\t";
+        cin>>u>>v;
+        adj2[v].push_back(u);
+        adj2[u].push_back(v);
+    }
+    
     
     return 0;
 }
